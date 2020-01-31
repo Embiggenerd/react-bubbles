81e17beaa263f304d0f805254c6af0f344d4fd79
igor <embiggenerd@gmail.com>
2020-01-31 11:33:08 -0500

    login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import axios from 'axios'
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+export const Login = () => {
+  const history = useHistory()
+
+  const [credentials, setCredentials] = useState({ username: "", password: "" })
+  const [loading, setLoading] = useState(false)
+
+  const [error, setError] = useState("")
+
+  const handleOnChange = (event) => {
+    setCredentials({
+      ...credentials,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleOnSubmit = async (event) => {
+    event.preventDefault()
+    setLoading(true)
+    try {
+
+      const { data } = await axios.post('http://localhost:5000/api/login', credentials)
+      localStorage.setItem('token', data.payload)
+      setCredentials({ username: "", password: "" })
+      setLoading(false)
+      history.push('/protected')
+    } catch (e) {
+      setLoading(false)
+      setError("Try Lambda School, i<3Lambd4")
+      setCredentials({ username: "", password: "" })
+      setTimeout(() => { setError("") }, 4000)
+    }
+  }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
-
-export default Login;
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleOnSubmit}>
+        <input onChange={handleOnChange} type="text" name="username" value={credentials.username} />
+        <input type="password" name="password" value={credentials.password} onChange={handleOnChange} />
+        <input type="submit" value="submit" />
+      </form>
+      <h4 className="error">{error}</h4>
+    </div>
+  )
+}
